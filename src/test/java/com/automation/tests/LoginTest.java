943aa979646210b520efd22fc355788ff674d7fb
SamCodes662 <sameerahuja662@gmail.com>
2025-12-26 16:09:51 -0500

    Initial setup for framework.Setup TestNG and gradle

---
+++ src/test/java/com/automation/tests/LoginTest.java
@@ -0,0 +1,18 @@
+package com.automation.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void testSuccessfulLogin() {
+//        LoginPage loginPage = new LoginPage(driver);
+//
+//        loginPage.enterUsername("standard_user");
+//        loginPage.enterPassword("secret_sauce");
+//        loginPage.clickLogin();
+//
+//        // Example Assertion: Verify URL changes after login
+//        Assert.assertTrue(driver.getCurrentUrl().contains("inventory.html"));
+    }
+}
